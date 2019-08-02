71d29141dd57060f080e7c1ba6d3773820977125
Chaitanya Joshi <joshi.chai@husky.neu.edu>
2019-08-02 13:22:02 -0400

    java script and menu btn changes

---
+++ files/js/main.js
@@ -0,0 +1,24 @@
+//select item from document object model
+const menuBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+const navItems = document.querySelectorAll('.nav-item');
+
+//set inital state of menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'));
+    }
+    else {
+
+    }
+}
