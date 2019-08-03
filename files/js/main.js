22fd996b306cd91312ff3b9f814c393eded2663d
Chaitanya Joshi <joshi.chai@husky.neu.edu>
2019-08-02 21:06:15 -0400

    Added other pages

--- files/js/main.js
+++ files/js/main.js
@@ -1,11 +1,10 @@
-//select item from document object model
 const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
 
-//set inital state of menu
+// Set Initial State Of Menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
@@ -17,8 +16,17 @@ function toggleMenu() {
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'));
-    }
-    else {
 
+        // Set Menu State
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'));
+
+        // Set Menu State
+        showMenu = false;
     }
 }
